cb69b52b3cb31b8db06e7eb205e6a0e6ae1f32e9
“Ygor <igorgarofalo@gmail.com>
2023-09-23 20:21:54 +0200

    added animation on cells

--- script/script.js
+++ script/script.js
@@ -10,6 +10,18 @@ const createTableGame = function () {
 createTableGame();
 
 const cells = document.getElementsByClassName("cells");
+
+const combinations = [
+  [0, 1, 2], //row
+  [3, 4, 5], //row
+  [6, 7, 8], //row
+  [0, 3, 6], //column
+  [1, 4, 7], //column
+  [2, 5, 8], //column
+  [0, 4, 8], //diagonal
+  [2, 4, 6], //diagonal
+];
+
 let isThisanX = true;
 
 for (let i = 0; i < cells.length; i++) {
